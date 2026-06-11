2afa7f3649054dbf6fea207e2a0a39986d410774
renatabagdanova <bagdanova.renata@gmail.com>
2026-06-11 19:31:58 +0300

    2

--- team_finder/settings.py
+++ team_finder/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = "team_finder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates_var1"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
